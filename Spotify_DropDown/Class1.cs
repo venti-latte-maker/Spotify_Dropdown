3a087ba1c9facb97ca993605990c8fc00ede4021
sunme <sunme@ZEPHY_G14>
2026-06-02 14:19:42 +0400

    Add project files.

---
+++ Spotify_DropDown/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotify_DropDown
+{
+    class Class1
+    {
+    }
+}
---
+++ Spotify_DropDown/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace Spotify_DropDown
+{
+
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+            Top = -Height;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Tick += CheckMouse;
+            timer.Start();
+        }
+
+        private void CheckMouse(object sender, EventArgs e)
+        {
+            var pos = Cursor.Position;
+
+        }
+    }
+}
